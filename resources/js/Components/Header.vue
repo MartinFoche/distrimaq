7d2c0d7a62e0dd17e84217776f5b1e4c0e164424
MartinFoche <fochesatto.mf@gmail.com>
2025-06-03 23:21:56 +0000

    Added home with first migrations of tables for products

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import "../../css/main.css";
+</script>
+<template>
+    <header class="header">
+        <Link class="header-title-distrimaq" href="/home">DISTRIMAQ</Link>
+    </header>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Authenticated from "@/Layouts/AuthenticatedLayout";
+import "../../css/main.css";
+</script>
+<template>
+    <Authenticated>
+        <h1>Holiwis</h1>
+    </Authenticated>
+</template>
